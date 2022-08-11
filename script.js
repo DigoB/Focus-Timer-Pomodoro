ce146231ed3f38d724b7a0a1ac94a1a5453e880f
Rodrigo Braz <braz.rodrigo94@gmail.com>
2022-08-11 17:17:52 -0300

    Inicio da implementação da lógica de tempo

--- script.js
+++ script.js
@@ -9,13 +9,36 @@ A DOM é programada direcionada a eventos(event-driven)
 Ao utilizar o querySelector podemos buscar os elementos no HTML 
  para fazer alterações, isso pode ocorrer tantos nas tags primárias
  como classes e ids que crie
-*/ 
-let buttonPlay = document.querySelector('.play')
-let buttonPause = document.querySelector('.pause')
-let buttonStop = document.querySelector('.stop')
-let buttonSet = document.querySelector('.set')
-let buttonSoundOn = document.querySelector('.sound-on')
-let buttonSoundOff = document.querySelector('.sound-off')
+*/
+const buttonPlay = document.querySelector('.play')
+const buttonPause = document.querySelector('.pause')
+const buttonStop = document.querySelector('.stop')
+const buttonSet = document.querySelector('.set')
+const buttonSoundOn = document.querySelector('.sound-on')
+const buttonSoundOff = document.querySelector('.sound-off')
+let minutes
+const minutesDisplay = document.querySelector('.minutes')
+let secondsDisplay = document.querySelector('.seconds')
+
+function countDown() {
+
+    // setTimeOut recebe uma função com o que é desejado que ela faça, e o segundo
+    // parametro é o tempo que ela vai executar
+    setTimeout(function() {
+        let seconds = Number(secondsDisplay.textContent)
+
+        if(seconds <= 0) {
+            seconds = 60
+        }
+
+        secondsDisplay.textContent = seconds - 1
+        
+        // A função chamando ela mesma se chama RECURSIVIDADE
+        // A cada segundo quando a função terminar, vai chamar a si mesma novamente
+        // Necessário implementar a lógica para quando isso vai parar.
+        countDown()        
+    }, 1000)
+}
 
 /**
  * Callback
@@ -30,6 +53,8 @@ buttonPlay.addEventListener('click', function() {
     buttonPause.classList.remove('hide')
     buttonStop.classList.remove('hide')
     buttonSet.classList.add('hide')
+
+    countDown()
 })
 
 buttonPause.addEventListener('click', function() {
@@ -54,4 +79,10 @@ buttonSoundOff.addEventListener('click', function() {
 buttonSoundOn.addEventListener('click', function() {
     buttonSoundOn.classList.add('hide')
     buttonSoundOff.classList.remove('hide')
+})
+
+buttonSet.addEventListener('click', function() {
+    minutes = prompt('Quantos minutos?')
+    // Para alterar o conteudo de um elemento HTML, podemos utilizar o textContent
+    minutesDisplay.textContent = minutes
 })
